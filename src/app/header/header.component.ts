f8d554e5f16463d63193f89a75b33b552aafa663
kieuPC <kieuPC@DESKTOP-6HTVATL>
2020-12-31 23:47:50 -0700

    add log out

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginDetailService } from '../shared/login-detail.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router,
+    public services: LoginDetailService) { }
 
   ngOnInit(): void {
   }
-
+//we need to add the function logout to navigate 
+onLogout() {
+  this.router.navigate(['/user/login']);//since the fullpatch is connect to the login the we can just navigate back to the login when the user want  to log out 
+}
 }
